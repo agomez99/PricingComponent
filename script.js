385a4e3eebc6155f2cf0e635128696b0c73db860
Austine Gomez <austine.gomez@yahoo.com>
2023-07-22 18:26:01 -0500

    mobile responsive

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+const toggle = document.getElementById("toggle");
+const basicPrice = document.getElementById("basicPrice");
+const professionalPrice = document.getElementById("proPrice");
+const masterPrice = document.getElementById("masterPrice");
+
+function setPrices(){
+  basicPrice.textContent = toggle.checked ? "$19.99" : "$199.99";
+  professionalPrice.textContent = toggle.checked ? "$24.99" : "$249.99";
+  masterPrice.textContent = toggle.checked ? "$39.99" : "$399.99";
+}
+
+setPrices();
+
+toggle.addEventListener("change", setPrices);
+
